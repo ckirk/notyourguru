5f2d1433d8d5514526ff864b10e56a398d2a1310
Chris <ckirkinis@gmail.com>
2024-03-21 13:10:34 -0700

    more cleanup

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,27 +1,29 @@
 import CMS from 'decap-cms-app'
 
+// Turn these back on and re-insall respective libraries via package.json to re-enable
 // import uploadcare from 'decap-cms-media-library-uploadcare'
 // import cloudinary from 'decap-cms-media-library-cloudinary'
 
 // Single Pages
 import IndexPagePreview from './preview-templates/IndexPagePreview'
-import AboutPagePreview from './preview-templates/AboutPagePreview'
+import SimplePagePreview from './preview-templates/SimplePagePreview'
 import RetreatsPagePreview from './preview-templates/RetreatsPagePreview'
 
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import RetreatPostPreview from './preview-templates/RetreatPostPreview'
 
-
+// Turn these back on and re-insall respective libraries via package.json to re-enable
 // CMS.registerMediaLibrary(uploadcare)
 // CMS.registerMediaLibrary(cloudinary)
 
 // Single Pages
 CMS.registerPreviewTemplate('index', IndexPagePreview)
-CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('retreats', RetreatsPagePreview) // using ProductPagePreview for now
-CMS.registerPreviewTemplate('training', AboutPagePreview) // using aboutPagePreview template for now
-CMS.registerPreviewTemplate('coaching', AboutPagePreview) // using aboutPagePreview template for now
-CMS.registerPreviewTemplate('programs', AboutPagePreview) // using aboutPagePreview template for now
+CMS.registerPreviewTemplate('retreats', RetreatsPagePreview)
+
+CMS.registerPreviewTemplate('about', SimplePagePreview)    // using common SimplePagePreview template for now
+CMS.registerPreviewTemplate('training', SimplePagePreview) // using common SimplePagePreview template for now
+CMS.registerPreviewTemplate('coaching', SimplePagePreview) // using common SimplePagePreview template for now
+CMS.registerPreviewTemplate('programs', SimplePagePreview) // using common SimplePagePreview template for now
 
 // Collections
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
--- src/cms/preview-templates/SimplePagePreview.js
+++ src/cms/preview-templates/SimplePagePreview.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+
+// Still using AboutPageTemplate here for simplicity
+// Will work as long as all simple pages share the same template
 import { AboutPageTemplate } from '../../templates/about-page'
 
-const AboutPagePreview = ({ entry, widgetFor }) => (
+const SimplePagePreview = ({ entry, widgetFor }) => (
   <AboutPageTemplate
     title={entry.getIn(['data', 'title'])}
     content={widgetFor('body')}
   />
 )
 
-AboutPagePreview.propTypes = {
+SimplePagePreview.propTypes = {
   entry: PropTypes.shape({
     getIn: PropTypes.func,
   }),
   widgetFor: PropTypes.func,
 }
 
-export default AboutPagePreview
+export default SimplePagePreview
