2cafc163b22f6d1e34dffd0ac48f83312cfab021
Chris <ckirkinis@gmail.com>
2024-03-21 13:04:41 -0700

    fixed previews

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,25 +1,28 @@
 import CMS from 'decap-cms-app'
+
 // import uploadcare from 'decap-cms-media-library-uploadcare'
 // import cloudinary from 'decap-cms-media-library-cloudinary'
 
-import AboutPagePreview from './preview-templates/AboutPagePreview'
-// import CoachingPagePreview from './preview-templates/CoachingPagePreview'
-// import ProgramsPagePreview from './preview-templates/ProgramsPagePreview'
-import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
+// Single Pages
 import IndexPagePreview from './preview-templates/IndexPagePreview'
+import AboutPagePreview from './preview-templates/AboutPagePreview'
+import RetreatsPagePreview from './preview-templates/RetreatsPagePreview'
+
+import BlogPostPreview from './preview-templates/BlogPostPreview'
 import RetreatPostPreview from './preview-templates/RetreatPostPreview'
 
 
 // CMS.registerMediaLibrary(uploadcare)
 // CMS.registerMediaLibrary(cloudinary)
 
+// Single Pages
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('coaching', AboutPagePreview) // using aboutPagePreview template for now
+CMS.registerPreviewTemplate('retreats', RetreatsPagePreview) // using ProductPagePreview for now
 CMS.registerPreviewTemplate('training', AboutPagePreview) // using aboutPagePreview template for now
+CMS.registerPreviewTemplate('coaching', AboutPagePreview) // using aboutPagePreview template for now
 CMS.registerPreviewTemplate('programs', AboutPagePreview) // using aboutPagePreview template for now
-// CMS.registerPreviewTemplate('products', ProductPagePreview)
-CMS.registerPreviewTemplate('retreats', ProductPagePreview) // using ProductPagePreview for now
+
+// Collections
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
 CMS.registerPreviewTemplate('retreat', RetreatPostPreview)
--- src/cms/preview-templates/RetreatsPagePreview.js
+++ src/cms/preview-templates/RetreatsPagePreview.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { ProductPageTemplate } from '../../templates/retreats-page'
+import { RetreatsPageTemplate } from '../../templates/retreats-page'
 
-const ProductPagePreview = ({ entry, getAsset }) => {
+const RetreatsPagePreview = ({ entry, getAsset }) => {
   const entryBlurbs = entry.getIn(['data', 'intro', 'blurbs'])
   const blurbs = entryBlurbs ? entryBlurbs.toJS() : []
 
@@ -13,7 +13,7 @@ const ProductPagePreview = ({ entry, getAsset }) => {
   const pricingPlans = entryPricingPlans ? entryPricingPlans.toJS() : []
 
   return (
-    <ProductPageTemplate
+    <RetreatsPageTemplate
       image={getAsset(entry.getIn(['data', 'image']))}
       title={entry.getIn(['data', 'title'])}
       heading={entry.getIn(['data', 'heading'])}
@@ -46,11 +46,11 @@ const ProductPagePreview = ({ entry, getAsset }) => {
   )
 }
 
-ProductPagePreview.propTypes = {
+RetreatsPagePreview.propTypes = {
   entry: PropTypes.shape({
     getIn: PropTypes.func,
   }),
   getAsset: PropTypes.func,
 }
 
-export default ProductPagePreview
+export default RetreatsPagePreview
